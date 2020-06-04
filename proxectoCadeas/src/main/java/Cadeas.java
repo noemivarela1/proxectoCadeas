efd695e84c7ea3f18fb0e460acb62ea7b45e3dee
Noemí <Noem@PortatilCasa>
2020-06-04 11:26:10 +0200

    Engadida funcionalidade de comprobación de se unha cadea é un número enteiro

--- proxectoCadeas/src/main/java/Cadeas.java
+++ proxectoCadeas/src/main/java/Cadeas.java
@@ -1,16 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 /**
  *
  * @author Noemí
  */
 public class Cadeas {
+    public static boolean eNumeroEnt(String str){
+        boolean eNum=true;
+        for (int i=0;i<str.length();i++){
+            if (str.charAt(i)>='0' && str.charAt(i)<='9'){
+                eNum=true;
+            }else{
+                eNum=false;
+                break;
+            }
+        }
+        return eNum;
+    }
     public static void main(String[] args){
-        System.out.println("Cadeas");
+        System.out.println("Proxecto Cadeas");
         System.out.println("Cambio feito en remoto");
+        String strNum="234";
+        System.out.println("A cadea é un número:"+eNumeroEnt(strNum));
     }
 }
